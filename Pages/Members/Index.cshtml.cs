f5c61ed621630097f731754377ff4a98d5173d1a
Malak <malakmetwallly@gmail.com>
2025-12-12 15:15:38 +0200

    Added some pages in the folders so they appear.

---
+++ Pages/Members/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FCMS_Project.Pages.Members
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
