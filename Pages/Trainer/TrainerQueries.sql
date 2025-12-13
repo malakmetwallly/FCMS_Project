06de11370e3c6f112245770c09b94d6d8d91d83e
Khaled Ameen <s-khaled.ameen@zewailcity.edu.eg>
2025-12-13 16:16:29 +0200

    TrainerQueries

--- Pages/Trainer/TrainerQueries.sql
+++ Pages/Trainer/TrainerQueries.sql
@@ -1,48 +1,77 @@
-﻿SELECT DISTINCT m.Member_ID, m.Name, m.Age, m.Goal
-FROM Member m
-JOIN Class_Enrollment ce ON m.Member_ID = ce.Member_ID
-WHERE ce.Trainer_ID = @Trainer_ID;
+﻿
+SELECT DISTINCT
+    M.Member_ID,
+    M.Name,               
+    M.Email,           
+    M.Goal                
+FROM
+    Member M
+JOIN
+    Class_Enrollment CE ON M.Member_ID = CE.Member_ID
+WHERE
+    CE.Trainer_ID = @TrainerID;
 
 
 
 
-INSERT INTO Workout_Plan (Member_ID, Trainer_ID, Weekly_Schedule, Focus_Area, Last_Updated)
-VALUES (@Member_ID, @Trainer_ID, @Weekly_Schedule, @Focus_Area, GETDATE());
-
-SELECT Plan_ID, Weekly_Schedule, Focus_Area, Last_Updated
-FROM Workout_Plan
-WHERE Member_ID = @Member_ID
-ORDER BY Last_Updated DESC;
-
-
-
-
-INSERT INTO Diet_Plan (Member_ID, Trainer_ID, Daily_Calories, Water_Intake_Goal, Allergies, Last_Updated)
-VALUES (@Member_ID, @Trainer_ID, @Daily_Calories, @Water_Intake_Goal, @Allergies, GETDATE());
-
-SELECT DietPlan_ID, Daily_Calories, Water_Intake_Goal, Allergies, Last_Updated
-FROM Diet_Plan
-WHERE Member_ID = @Member_ID
-ORDER BY Last_Updated DESC;
+SELECT
+    Name
+FROM
+    Member
+WHERE
+    Member_ID = @MemberID;
 
 
 
 
-INSERT INTO Progress (Member_ID, Trainer_ID, Weight, Muscle_Mass, Body_Fat_Percentage, Notes)
-VALUES (@Member_ID, @Trainer_ID, @Weight, @Muscle_Mass, @Body_Fat_Percentage, @Notes);
 
-SELECT Date_Recorded, Weight, Muscle_Mass, Body_Fat_Percentage, Notes
-FROM Progress
-WHERE Member_ID = @Member_ID
-ORDER BY Date_Recorded;
+INSERT INTO Workout_Plan (
+    Member_ID, 
+    Trainer_ID, 
+    Weekly_Schedule, 
+    Focus_Area, 
+    Last_Updated
+)
+VALUES (
+    @MemberID, 
+    @TrainerID, 
+    @WeeklySchedule, 
+    @FocusArea,       
+    GETDATE()
+);
 
 
 
+SELECT 
+    Date_Recorded, 
+    Weight, 
+    Muscle_Mass, 
+    Body_Fat_Percentage, 
+    Notes
+FROM 
+    Progress
+WHERE 
+    Member_ID = @MemberID
+ORDER BY 
+    Date_Recorded DESC;
 
-INSERT INTO Feedback (Member_ID, Message, Type, Trainer_ID, Admin_ID)
-VALUES (@Member_ID, @Message, @Type, @Trainer_ID, NULL);
 
-SELECT Message, Type, Date_Submitted
-FROM Feedback
-WHERE Member_ID = @Member_ID
-ORDER BY Date_Submitted DESC;
+
+INSERT INTO Progress (
+    Member_ID, 
+    Trainer_ID, 
+    Date_Recorded, 
+    Weight, 
+    Muscle_Mass, 
+    Body_Fat_Percentage, 
+    Notes
+)
+VALUES (
+    @MemberID, 
+    @TrainerID, 
+    GETDATE(), 
+    @Weight, 
+    @MuscleMass,       
+    @BodyFatPercentage, 
+    @Notes
+);
